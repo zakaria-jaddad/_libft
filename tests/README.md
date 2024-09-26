3bd00fe5f8def278c9da9d74415b0a7e4d5e5ea5
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-09-26 18:35:29 +0100

    Tests: Add criterion source code + ft_memset tests

---
+++ tests/README.md
@@ -0,0 +1,8 @@
+# libft tests
+
+- Tests using criterion
+
+```bash
+    make all # to run the tests for the first time
+    make re  # to recompile libft and run tests
+```
--- tests/include/tests.h
+++ tests/include/tests.h
@@ -2,7 +2,10 @@
 # define TESTS_H
 
 # include "../../libft.h"
-# include </opt/homebrew/include/criterion/criterion.h>
+# include "../criterion/criterion.h" 
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
 # include <strings.h>
 
 #endif
